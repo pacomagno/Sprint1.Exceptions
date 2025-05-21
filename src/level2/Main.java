be0414fc940cb1a0db2731c670e8db071693fade
BitteNails <pacomagnoiii@gmail.com>
2025-05-21 12:30:15 +0200

    minor chages in code

--- src/level2/Main.java
+++ src/level2/Main.java
@@ -7,7 +7,7 @@ import level2.model.Input;
 public class Main {
     public static void main(String[] args) {
             ProgramRunner runner = new ProgramRunner();
-            runner.run(); // Ejecutar el programa
+            runner.run(); 
         }
     }
 
